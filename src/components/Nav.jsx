a6eb71bb762c78c1d91baaa560348b9c34cbb720
EvanA <evanabbott04@gmail.com>
2024-04-12 15:41:43 -0400

    more nav reactivity

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,35 +8,33 @@ export default function Nav(props) {
     const pgStates = ['pg', 'openPg', 'closePg'];
 
     return (
-        <div className='navContainer'>
-            <div className={pgStates[navOpen]}>
-                <div className='hideLinks'>
-                    <div className='navLinks'>
-                            <a href="#About" onClick={()=>{
-                                props.setDefault(1);
-                                setNav(2);
-                            }}><img className='icon' src="./src/assets//questionMark.svg"/>About</a>
+        <div className={pgStates[navOpen]}>
+            <div className='hideLinks'>
+                <div className='navLinks'>
+                        <a href="#About" onClick={()=>{
+                            props.setDefault(1);
+                            setNav(2);
+                        }}><img className='icon' src="./src/assets//questionMark.svg"/>About</a>
 
-                            <a href="#Experience" onClick={()=>{
-                                props.setDefault(1);
-                                setNav(2);
-                            }}><img className='icon' src="./src/assets/book.svg"/>Experience</a>
+                        <a href="#Experience" onClick={()=>{
+                            props.setDefault(1);
+                            setNav(2);
+                        }}><img className='icon' src="./src/assets/book.svg"/>Experience</a>
 
-                            <a href="#Contact" onClick={()=>{
-                                setNav(2);
-                            }}><img className='icon' src="./src/assets/magnifyingGlass.svg"/>Contact</a>
+                        <a href="#Contact" onClick={()=>{
+                            setNav(2);
+                        }}><img className='icon' src="./src/assets/magnifyingGlass.svg"/>Contact</a>
 
-                            <a href="#Projects" onClick={()=>{
-                                props.setDefault(2);
-                                setNav(2);
-                            }}><img className='icon' src="./src/assets/hammer.svg"/>Projects</a>
-                    </div>
+                        <a href="#Projects" onClick={()=>{
+                            props.setDefault(2);
+                            setNav(2);
+                        }}><img className='icon' src="./src/assets/hammer.svg"/>Projects</a>
                 </div>
-                <button className={btnStates[navOpen]} onClick={()=>{
-                    if (navOpen % 2 == 0) setNav(1);
-                    else setNav(2);
-                }}><img src="./src/assets/bookmark.svg"/></button>
             </div>
+            <button className={btnStates[navOpen]} onClick={()=>{
+                if (navOpen % 2 == 0) setNav(1);
+                else setNav(2);
+            }}><img src="./src/assets/bookmark.svg"/></button>
         </div>
     )
 }
